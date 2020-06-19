5ea0ef8297230916a070fe7884c5a4e578bcd416
yo-fenix <alejandro.iglesias@trifenix.io>
2020-06-19 19:43:02 -0400

    Inicio Generics Marvel

---
+++ Marvel-Generics/Marvel-Generics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Marvel_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
