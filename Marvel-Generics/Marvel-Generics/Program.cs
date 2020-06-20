4f08f35f69ce6abb56ab9b32487a4c0b52a2979e
yo-fenix <alejandro.iglesias@trifenix.io>
2020-06-19 20:06:41 -0400

    Modelo semi-completado

--- Marvel-Generics/Marvel-Generics/Program.cs
+++ Marvel-Generics/Marvel-Generics/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 
 namespace Marvel_Generics
 {
@@ -10,5 +11,49 @@ namespace Marvel_Generics
             // mensaje 
             Console.WriteLine("Hello World!");
         }
+
+        public interface IHeroe : IPersonaConPoderes {
+            public PersonaComunYCorriente Mama { get; set; }
+        }
+
+        public class  PersonaComunYCorriente  {
+            public DateTime? FechaNacimiento { get; set; }
+        }
+
+        public class Animal {
+            public string Identificador { get; set; }
+
+            public string Nombre { get; set; }
+        }
+
+
+
+        public interface IPersonaConPoderes {
+            public string NombreFantasia { get; set; }
+
+            public string NombreReal { get; set; }
+        }
+
+        public interface IVillano : IPersonaConPoderes {
+
+            public Animal Mascota { get; set; }
+        }
+
+        public class Poder { 
+            
+        }
+
+
+        public enum TipoPoder { 
+            Hielo = 0,
+            Fuego = 1,
+            SuperInteligencia =2,
+            HinchaWea = 3, 
+            ControlMental =4, 
+            DestruyeAlmas =5
+        }
+
+        
+
     }
 }
